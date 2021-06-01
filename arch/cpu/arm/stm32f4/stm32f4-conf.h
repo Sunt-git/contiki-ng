e9c0c5b6e9c7d124c2357f84298a23caef7a1e61
sunt <suntian587@163.com>
2021-06-01 20:07:38 +0800

    configure

--- arch/cpu/arm/stm32f4/stm32f4-conf.h
+++ arch/cpu/arm/stm32f4/stm32f4-conf.h
@@ -1,7 +1,7 @@
 #ifndef STM32F4_CONF_H_
 #define STM32F4_CONF_H_
 
-#define LINKADDR_CONF_SIZE 6
+#define LINKADDR_CONF_SIZE 8
 
 #ifndef NETSTACK_CONF_RADIO
 #define NETSTACK_CONF_RADIO         stm32f4_rf_driver
